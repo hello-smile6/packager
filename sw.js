d6da45af651036792645308cb5ca839a462068fc
GarboMuffin <GarboMuffin@users.noreply.github.com>
2022-01-09 17:01:08 +0000

    Deploying to gh-pages from @ TurboWarp/packager@5fc2b11cb8adfccacb9bea8437407d547ded525e 🚀

---
+++ sw.js
@@ -0,0 +1,50 @@
+// These will be replaced at build-time by generate-service-worker-plugin.js
+const ASSETS = ["","js/sha256.52086ae4fb19cece366a.worker.js","js/download-project.36aab6a3ee2a3603bd9d.worker.js","assets/reset.80a6e1615fc013684ad8047dba5ce064.svg","assets/default-icon.290e09e569a1cab8e61ba93b0d23863f.png","js/vendors~icns~jszip.57d38736775d32112049.js","js/icns.cdd8590f4c968977a347.js","js/jszip.43b361b27d567cd225a8.js","js/p4.c91a16e233ec22fd079b.js","js/packager-options-ui.dc8c2937ea8793078759.js"];
+const CACHE_NAME = "p4-65af0b0bec33e9d8e257257d3e7731978d9ee210e9f0673b76b1cee6dba289d6";
+const IS_PRODUCTION = true;
+
+const base = location.pathname.substr(0, location.pathname.indexOf('sw.js'));
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS.map(i => i === '' ? base : i))));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(i => i !== CACHE_NAME).map(i => caches.delete(i))))
+  );
+});
+
+const fetchWithTimeout = (req) => new Promise((resolve, reject) => {
+  const timeout = setTimeout(reject, 5000);
+  fetch(req)
+    .then((res) => {
+      clearTimeout(timeout);
+      resolve(res);
+    })
+    .catch((err) => {
+      clearTimeout(timeout);
+      reject(err);
+    });
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
+  const relativePathname = url.pathname.substr(base.length);
+  if (IS_PRODUCTION && ASSETS.includes(relativePathname)) {
+    url.search = '';
+    const immutable = !!relativePathname;
+    if (immutable) {
+      event.respondWith(
+        caches.match(new Request(url)).then((res) => res || fetch(event.request))
+      );
+    } else {
+      event.respondWith(
+        fetchWithTimeout(event.request).catch(() => caches.match(new Request(url)))
+      );
+    }
+  }
+});
